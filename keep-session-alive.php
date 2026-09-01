08ee09da70ea7f99427700dc4a73f7e28d75caef
Carlos Tão <113472199+813648@users.noreply.github.com>
2026-09-01 18:57:11 +0100

    Delete keep-session-alive.php

--- keep-session-alive.php
+++
@@ -1,9 +0,0 @@
-<?php
-// keep-session-alive.php
-// Responde com 204 No Content para pings keep-alive
-http_response_code(204);
-header('Cache-Control: no-store, no-cache, must-revalidate');
-header('Expires: 0');
-header('X-Content-Type-Options: nosniff');
-    echo "<div class='error'>Acesso inválido. Por favor, use o formulário para enviar a mensagem.</div>";
-exit;
